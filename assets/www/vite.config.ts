6b558ba669635c2960f77d5e028ef27c713e2dff
kerilov1138 <aksahinkerem@gmail.com>
2026-04-20 11:25:23 +0300

    Bundle application files (Static Mirror)

--- assets/www/vite.config.ts
+++ assets/www/vite.config.ts
@@ -1,20 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
-    base: './',
+    plugins: [react(), tailwindcss(), viteSingleFile()],
     build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      emptyOutDir: true,
+      reportCompressedSize: false,
+      cssCodeSplit: false,
+      assetsInlineLimit: 100000000,
       rollupOptions: {
         output: {
-          manualChunks: undefined,
+          inlineDynamicImports: true,
         },
       },
     },
